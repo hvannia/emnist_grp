3a07887418f2ff8ac0038f1c1e3b0911a285ff1f
vannia <hvannia@gmail.com>
2019-04-26 12:19:54 -0700

    added pen size slider

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -1,18 +1,24 @@
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
-canvas.width = 280;
-canvas.height = 280;
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-ctx.strokeStyle = 'white';
-ctx.lineJoin = 'round';
-ctx.lineCap = 'round';
-ctx.lineWidth = 17;
-
+const slider = document.querySelector('#wSlider');
+const output= document.querySelector('#widthVal');
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 var gdata;
+
+function initCanvas(){
+    canvas.width = 280;
+    canvas.height = 280;
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = 'white';
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    ctx.lineWidth = 10;
+}
+
+
 
 function clearme(){
 	ctx.clearRect(0,0,canvas.width, canvas.height);
@@ -36,11 +42,6 @@ function calculate(){
 
 
     });
-    /* for debugging only 
-    w=window.open('about:blank','image from canvas');
-    w.document.write("<img src='"+d+"' alt='from canvas'/>");
-    w.document.body.style.background = 'blue';
-    ***********************/
 }
 
 
@@ -62,9 +63,21 @@ canvas.addEventListener('mousedown', (e) => {
 });
 
 
+function initPage(){
+    document.querySelector('#originalCard').style.display="none";
+    document.querySelector('#processedCard').style.display="none";
+    
+    output.innerText = slider.value;
+    
+    slider.oninput = function() {
+      output.innerText = this.value;
+      ctx.lineWidth = this.value;
+    }
+    
+    canvas.addEventListener('mousemove', draw);
+    canvas.addEventListener('mouseup', () => isDrawing = false);
+    canvas.addEventListener('mouseout', () => isDrawing = false);
+}
 
-document.querySelector('#originalCard').style.display="none";
-document.querySelector('#processedCard').style.display="none";
-canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mouseup', () => isDrawing = false);
-canvas.addEventListener('mouseout', () => isDrawing = false);
+initCanvas();
+initPage();
